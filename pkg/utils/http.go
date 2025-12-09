27a66cf9a7e2e76f758c3bf5013e0be61cda411a
lihaipeng@uniontech.com <lihaipeng@uniontech.com>
2025-12-09 16:43:58 +0800

    增加 HTTPS 证书校验开关和默认 URI 配置。

--- pkg/utils/http.go
+++ pkg/utils/http.go
@@ -9,6 +9,9 @@ import (
 
 func ValidateURI(uri string) error {
         parsedURL, err := url.ParseRequestURI(uri)
+        if err != nil {
+                return fmt.Errorf("invalid URI format: %w", err)
+        }
 
         if parsedURL.Scheme != "http" && parsedURL.Scheme != "https" {
                 return fmt.Errorf("unsupported scheme: %s", parsedURL.Scheme)
