41fda5eb221c7bd8e9b0f810967c2114f4194dbf
lihaipeng@uniontech.com <lihaipeng@uniontech.com>
2026-03-04 15:59:52 +0800

    新增指标注册、获取及 Prometheus 采集支持

--- internal/exporter/registry.go
+++ internal/exporter/registry.go
@@ -3,8 +3,11 @@
 package exporter
 
 import (
+    "github.com/prometheus/client_golang/prometheus"
     "sync"
 )
+
+var defaultReg *Registry
 
 type Registry struct {
 	metrics []Metric
@@ -25,3 +28,23 @@ func RegisterPrometheus(reg *prometheus.Registry) {
 	reg.MustRegister(defaultReg)
 }
 
+func (r *Registry) Register(metrics Metric) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.metrics = append(r.metrics, metrics)
+}
+
+func (r *Registry) GetMetrics() []Metric {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+	return r.metrics
+}
+
+func (r *Registry) Describe(descs chan<- *prometheus.Desc) {
+}
+
+func (r *Registry) Collect(ch chan<- prometheus.Metric) {
+	for _, m := range r.GetMetrics() {
+		m.Collect(ch)
+	}
+}
