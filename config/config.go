6bf3b864b1294c181e6f087219196a6ba9b1d343
lihaipeng@uniontech.com <lihaipeng@uniontech.com>
2026-04-10 15:31:45 +0800

    统一缩进格式

--- config/config.go
+++ config/config.go
@@ -3,43 +3,42 @@
 package config
 
 import (
-        "uos-mgmt-exporter/pkg/utils"
-        "github.com/alecthomas/kingpin"
-        "github.com/sirupsen/logrus"
+	"github.com/alecthomas/kingpin"
+	"github.com/sirupsen/logrus"
+	"uos-mgmt-exporter/pkg/utils"
 )
 
 var (
-        ScrapeUrl       *string
-        Insecure        *bool
-        DefaultSettings = Settings{
-                //ScrapeUri: "http://127.0.0.1:24220/api/plugins.json",
-                Insecure: false,
-        }
+	ScrapeUrl       *string
+	Insecure        *bool
+	DefaultSettings = Settings{
+		//ScrapeUri: "http://127.0.0.1:24220/api/plugins.json",
+		Insecure: false,
+	}
 )
 
 func init() {
-        ScrapeUrl = kingpin.Flag("scrape_uri",
-                "Scrape URI").
-                Short('s').
-                String()
-        Insecure = kingpin.Flag("insecure",
-                "Ignore server certificate if using https, Default: false.").
-                Bool()
-        if *ScrapeUrl != "" {
-                if err := utils.ValidateURI(*ScrapeUrl); err != nil {
-                        logrus.Warnf("Invalid scrape uri: %s", err)
-                        logrus.Warnf("Use default scrape uri: %s", DefaultSettings.ScrapeUri)
-                        *ScrapeUrl = DefaultSettings.ScrapeUri
-                }
-        }
+	ScrapeUrl = kingpin.Flag("scrape_uri",
+		"Scrape URI").
+		Short('s').
+		String()
+	Insecure = kingpin.Flag("insecure",
+		"Ignore server certificate if using https, Default: false.").
+		Bool()
+	if *ScrapeUrl != "" {
+		if err := utils.ValidateURI(*ScrapeUrl); err != nil {
+			logrus.Warnf("Invalid scrape uri: %s", err)
+			logrus.Warnf("Use default scrape uri: %s", DefaultSettings.ScrapeUri)
+			*ScrapeUrl = DefaultSettings.ScrapeUri
+		}
+	}
 
-        if *Insecure {
-                logrus.Warn("Insecure mode enabled, this is not recommended for production use.")
-        }
+	if *Insecure {
+		logrus.Warn("Insecure mode enabled, this is not recommended for production use.")
+	}
 }
 
 type Settings struct {
-        ScrapeUri string `yaml:"scrape_uri"`
-        Insecure  bool   `yaml:"insecure"`
+	ScrapeUri string `yaml:"scrape_uri"`
+	Insecure  bool   `yaml:"insecure"`
 }
-
