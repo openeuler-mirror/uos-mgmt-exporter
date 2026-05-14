59d69fc42bde47ca9005c2be45f72cf4fc6f74c8
liweigang <liweiganga@uniontech.com>
2026-05-14 20:38:12 +0800

    metrics: add listen field and fix mutex indentation in Prometheus struct

--- internal/metrics/mgmt.go
+++ internal/metrics/mgmt.go
@@ -25,6 +25,8 @@ type resStateWithKind struct {
 }
 
 type Prometheus struct {
+	listen string
+
 	// 指标
 	checkApplyTotal        *prometheus.CounterVec
 	pgraphStartTimeSeconds prometheus.Gauge
@@ -34,7 +36,7 @@ type Prometheus struct {
 
 	// 状态管理
 	resourcesState map[string]resStateWithKind
-    mutex          sync.Mutex
+	mutex          sync.Mutex
 }
 
 func (p *Prometheus) Describe(ch chan<- *prometheus.Desc) {
