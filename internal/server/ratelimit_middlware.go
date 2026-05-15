d14e20242f11971e5792a25dc949acb14b8bd02e
liweigang <liweiganga@uniontech.com>
2026-05-15 09:26:31 +0800

    server: add init() to register ratelimit kingpin flags

--- internal/server/ratelimit_middlware.go
+++ internal/server/ratelimit_middlware.go
@@ -16,6 +16,15 @@ var (
 	UseRatelimit      *bool
 )
 
+func init() {
+	rateLimitInterval = kingpin.Flag("rate_limit_interval",
+		"rate limit interval").Default("1s").Duration()
+	rateLimitSize = kingpin.Flag("rate_limit_size",
+		"rate limit size").Default("100").Int()
+	UseRatelimit = kingpin.Flag("use_ratelimit",
+		"use rate limit").Bool()
+}
+
 func Ratelimit(ratelimiter *ratelimit.RateLimiter) HandlerFunc {
 	logrus.Info("ratelimit middleware init")
 	logrus.Debugf("ratelimit middleware init rateLimitInterval: %v, rateLimitSize: %v\n", *rateLimitInterval, *rateLimitSize)
