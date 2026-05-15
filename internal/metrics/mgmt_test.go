38a058601fa85d01ef532133ba2ffdb960772d0a
liweigang <liweiganga@uniontech.com>
2026-05-15 09:37:43 +0800

    metrics: add TestPrometheusMetrics function start

--- internal/metrics/mgmt_test.go
+++ internal/metrics/mgmt_test.go
@@ -6,3 +6,11 @@ import (
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+// TestPrometheusMetrics 测试 Prometheus 指标是否正确更新
+func TestPrometheusMetrics(t *testing.T) {
+	// 初始化 Prometheus 收集器
+	collector := NewMgmtCollect()
+
